065bd32fc0f716787635aca7e4bd739c79e4d9a1
iamjai-dev <jayaprakash@altrosyn.com>
2022-11-04 13:07:59 +0530

    added-joi-for-validating-api-requests

--- src/routes/Author.routes.ts
+++ src/routes/Author.routes.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import AuthorController from '../controllers/AuthorController';
+import { Schemas, ValidateSchema } from '../middleware/ValidateSchema';
 
 const router = express.Router();
 
-router.post('/create', AuthorController.createAuthor);
+router.post(
+  '/create',
+  ValidateSchema(Schemas.author.create),
+  AuthorController.createAuthor
+);
 router.get('/get/:authorId', AuthorController.readAuthor);
 router.get('/get', AuthorController.readAll);
-router.patch('/update/:authorId', AuthorController.updateAuthor);
+router.patch(
+  '/update/:authorId',
+  ValidateSchema(Schemas.author.update),
+  AuthorController.updateAuthor
+);
 router.delete('/delete/:authorId', AuthorController.deleteAuthor);
 
 export = router;
--- src/routes/Book.routes.ts
+++ src/routes/Book.routes.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import BookController from '../controllers/BookController';
+import { Schemas, ValidateSchema } from '../middleware/ValidateSchema';
 
 const router = express.Router();
 
-router.post('/create', BookController.createBook);
+router.post(
+  '/create',
+  ValidateSchema(Schemas.book.create),
+  BookController.createBook
+);
 router.get('/get/:bookId', BookController.readBook);
 router.get('/get', BookController.readAll);
-router.patch('/update/:bookId', BookController.updateBook);
+router.patch(
+  '/update/:bookId',
+  ValidateSchema(Schemas.book.update),
+  BookController.updateBook
+);
 router.delete('/delete/:bookId', BookController.deleteBook);
 
 export = router;
